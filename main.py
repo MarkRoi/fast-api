4d974da0b507cb0a1e1f9e5288908212b2689f69
Kalema Mark <kalemamark46@gmail.com>
2025-06-09 20:17:34 +0100

    Creating routes

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
+
+items = []
+
 
 @app.get("/")
 def root():
     return {"message": "Hello, World!"}
+
+@app.post("/items")
+def create_item(item:str):
+    items.append(item)
+    return {"message": "Item created successfully", "item": items}
+
+@app.get("/items/{item_id}")
+def get_item(item_id:int):
+    if item_id < len(items):
+        return {"item": items[item_id]}
+    else:
+        raise HTTPException(status_code=404, detail="Item not found")
