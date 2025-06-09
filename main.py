9e97da92daabd8b0c19eeff00eca286883f2d8b6
Kalema Mark <kalemamark46@gmail.com>
2025-06-09 20:29:03 +0100

    Request and payload

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Item(BaseModel):
+    text: str = None
+    is_done: bool = False
 
 items = []
 
@@ -10,9 +15,13 @@ def root():
     return {"message": "Hello, World!"}
 
 @app.post("/items")
-def create_item(item:str):
+def create_item(item:Item):
     items.append(item)
     return {"message": "Item created successfully", "item": items}
+
+@app.get("/items")
+def list_item(limit:int = 10):
+    return items[0:limit]
 
 @app.get("/items/{item_id}")
 def get_item(item_id:int):
